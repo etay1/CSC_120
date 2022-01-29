8e2f222d32540655658c860641e74fe38b0d211c
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-01-29 17:16:07 -0500

    Update RandomCharacter.java

--- Personal Practice/RandomCharacter.java
+++ Personal Practice/RandomCharacter.java
@@ -1,1 +1,11 @@
+public class RandomCharacter {
+    public static void main (String [] args) {
+		char ch = getRandomCharacter ('\u0000', '\uFFFF');
+		System.out.println (ch);
+	}
 
+	public static char getRandomCharacter (char ch1, char ch2) {
+		return (char) (ch1 + Math.random () * (ch2 - ch1 + 1));
+	}
+  
+}
