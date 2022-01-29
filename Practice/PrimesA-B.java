ee482a17c21b3e810fd3d1b21591a2e845251637
Elijah Tay <98202550+etay1@users.noreply.github.com>
2022-01-29 17:23:41 -0500

    Create PrimesA-B.java

---
+++ Practice/PrimesA-B.java
@@ -0,0 +1,26 @@
+public class Primes {
+    public static void main (String [] args) {
+        final int NUM = 50;
+        final int PER_LINE = 10;
+        int count = 0;
+        int n = 2;
+        while (count < NUM)
+        {
+            boolean isPrime = true;
+            for (int d = 2; d < n && isPrime; d++)
+            {
+                if (n % d == 0)
+                    isPrime = false;
+            }
+            if (isPrime)
+            {
+                count++;
+                if (count % PER_LINE == 0)
+                    System.out.println (n);
+                else
+                    System.out.print (n + "\t");
+            }
+            n++;	
+        }
+    }
+}
